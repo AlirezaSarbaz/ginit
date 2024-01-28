2ed2b0505396f14cf35c3cdd17f1dbee11a692d9
AlirezaSarbaz <alirezasarbaz2aq5@gmail.com>
2024-01-29 01:35:38 +0330

    create a function to check .ginit is exist or not and create primary run_init function

--- functions.c
+++ functions.c
@@ -7,3 +7,43 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#define MAX_ADDRESS_LENGHT 1024
+
+int check_ginit_exist();
+void run_init();
+
+int check_ginit_exist() {
+    char cwd[MAX_ADDRESS_LENGHT];
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        perror("getcwd\n");
+        exit(EXIT_FAILURE);
+    }
+    char tmp_cwd[MAX_ADDRESS_LENGHT];
+    bool exists = false;
+    struct dirent *entry;
+    do {
+        DIR *dir = opendir(".");
+        if (dir == NULL) {
+            perror("Error opening current directory");
+            exit(EXIT_FAILURE);
+        }
+        while ((entry = readdir(dir)) != NULL) {
+            if (entry->d_type == DT_DIR && !strcmp(entry->d_name, ".ginit"))
+                exists = true;
+        }
+        closedir(dir);
+        if (getcwd(tmp_cwd, sizeof(tmp_cwd)) == NULL) exit(EXIT_FAILURE);
+        if (strcmp(tmp_cwd, "/")) {
+            if (chdir("..")) exit(EXIT_FAILURE);
+        }
+    } while (strcmp(tmp_cwd, "/"));
+    if (chdir(cwd)) {
+        perror("chdir\n");
+        exit(EXIT_FAILURE);
+    }
+    if (exists) return 1;
+    else return 0;
+}
+void run_init() {
+    mkdir(".ginit" , 0755);
+}
--- main.c
+++ main.c
@@ -1,5 +1,18 @@
-#include <stdio.h>
 #include "functions.c"
+
 int main(int argc , char* argv[]) {
+    if (argc < 2) {
+        perror("please enter a valid command\n");
+        return 1;
+    }
+    else if (!strcmp(argv[1] , "init")) {
+        if (!check_ginit_exist()) {
+            run_init();
+        }
+        else {
+            perror("ginit repository has already initialized\n");
+            return 1;
+        }
+    }
     return 0;
 }
