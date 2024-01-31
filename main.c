c4a702be40eb111c168a4f7b887ba7ee69491a34
AlirezaSarbaz <alirezasarbaz2aq5@gmail.com>
2024-01-31 13:34:25 +0330

    some functions added and some buges fixed

--- main.c
+++ main.c
@@ -1,22 +1,29 @@
 #include "functions.c"
-#include <dirent.h>
 
 int main(int argc , char* argv[]) {
     if (argc < 2) {
         perror("please enter a valid command\n");
         return 1;
     }
-    else if (!strcmp(argv[1] , "init")) {
-        if (!check_ginit_exist()) {
-            run_init();
+    else {
+        if (!strcmp(argv[1] , "init")) {
+            if (!check_ginit_exist()) {
+                run_init();
+                FILE* file = fopen(".ginit/time" , "w+");
+                fprintf(file , "%ld" , time(NULL)); fclose(file);
+                return 0;
+            }
+            else {
+                perror("ginit repository has already initialized\n");
+                return 1;
+            }
         }
-        else {
-            perror("ginit repository has already initialized\n");
-            return 1;
+        else if (!strcmp(argv[1] , "add")) {
+            run_add(argc , argv);
         }
+        update_stages();
     }
-    else if (!strcmp(argv[1] , "add")) {
-        run_add(argc , argv);
-    }
+    FILE* file = fopen(".ginit/time" , "w+");
+    fprintf(file , "%ld" , time(NULL)); fclose(file);
     return 0;
 }
