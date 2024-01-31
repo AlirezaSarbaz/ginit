f2be0565a9eed600119635d2652ac005ff79b664
AlirezaSarbaz <alirezasarbaz2aq5@gmail.com>
2024-02-01 00:55:27 +0330

    update functions

--- functions.c
+++ functions.c
@@ -10,10 +10,13 @@
 
 #define MAX_ADDRESS_LENGTH 1024
 #define MAX_LINE_LENGTH 1024
+#define MAX_BRANCH_NAME 100
+#define MAX_EMAIL_USERNAME_LENGHT 100
+#define MAX_FILENAME 100
 
 int check_ginit_exist();
 void run_init();
-void copy_file_source_to_dest(FILE* dest , FILE* source);
+void copy_file_source_to_dest(FILE* dest , FILE* src);
 char* generate_commit_id();
 
 void run_add(int argc, char *const argv[]);
@@ -24,6 +27,7 @@ int check_files_modified(const char* file_path);
 void update_stages();
 void run_commit(int argc , char* argv[]);
 int is_stage(char* pathspec);
+void add_to_logs(char* argv[] , const char* commit_id);
 
 int check_ginit_exist() {
     char cwd[MAX_ADDRESS_LENGTH];
@@ -71,16 +75,16 @@ void run_init() {
     //file = fopen(".ginit/filesdata" , "w"); fclose(file);
     file = fopen(".ginit/time" , "w"); fclose(file);
     file = fopen(".ginit/HEAD" , "w"); fprintf(file , "00000000 master");fclose(file);
-    file = fopen(".ginit/logs" , "w"); fprintf(file , "00000000"); fclose(file);
+    file = fopen(".ginit/logs" , "w"); fclose(file);
     mkdir(".ginit/commits" , 0755);
 }
-void copy_file_source_to_dest(FILE* dest , FILE* source) {
+void copy_file_source_to_dest(FILE* dest , FILE* src) {
     char buffer[MAX_LINE_LENGTH];
     size_t bytes_read;
-    while ((bytes_read = fread(buffer, 1, sizeof(buffer), source)) > 0) {
+    while ((bytes_read = fread(buffer, 1, sizeof(buffer), src)) > 0) {
         fwrite(buffer, 1, bytes_read, dest);
     }
-    fclose(dest); fclose(source);
+    fclose(dest); fclose(src);
 }
 void run_add(int argc, char *const argv[]) {
     // TODO: handle command in non-root directories 
@@ -153,15 +157,14 @@ void list_files_recursively(const char* basePath , const char* filename , int de
         if (entry->d_type == DT_DIR) {
             if (strcmp(entry->d_name , ".") && strcmp(entry->d_name , "..")) {
                 sprintf(path , "%s/%s" , basePath , entry->d_name);
-                fprintf(file , "%s %s %d\n" , entry->d_name ,path , depth);
+                fprintf(file , "%s %s %d\n" , entry->d_name ,path , depth); fclose(file);
                 list_files_recursively(path , filename , depth + 1);
             }
         }
         else {
             sprintf(path , "%s/%s" , basePath , entry->d_name);
-            fprintf(file , "%s %s %d\n" , entry->d_name ,path, depth);
+            fprintf(file , "%s %s %d\n" , entry->d_name ,path, depth); fclose(file);
         }
-        fclose(file);
     }
     closedir(dir);
 }
@@ -233,17 +236,21 @@ void update_stages() {
     return 0; 
 }
 void run_commit(int argc , char* argv[]) {
-    char path[MAX_ADDRESS_LENGTH] , commit_id[9];
+    char path[MAX_ADDRESS_LENGTH] , commit_id[9] , line[MAX_LINE_LENGTH] , current_commit_id[9] , current_branch[MAX_BRANCH_NAME];
     strcpy(commit_id , generate_commit_id());
     sprintf(path , ".ginit/commits/%s" , commit_id);
     mkdir(path , 0755);
     add_to_logs(argv , commit_id);
-    
+    FILE* head = fopen(".ginit/HEAD" , "r+");
+    fgets(line , sizeof(line) , head);
+    sscanf(line , "%s %s" , current_commit_id , current_branch);
+    fseek(head , 0 , SEEK_SET); fprintf(head , "%s %s" , commit_id , current_branch);fclose(head);
+    copy_stagedfiles_to_commit_dir(commit_id);
 }
 char* generate_commit_id() {
     char* result = malloc(9 * sizeof(char));
     srand((unsigned int)time(NULL));
-    const char chars[] = {"ABCDEFGHIJKLMNOPKRSTUVWXYZ1234567890"};
+    const char chars[] = {"abcdefghijklmnopqrstuvwxyz1234567890"};
     for (int i = 0; i < 8; i++) {
         int random_index = rand() % (sizeof(chars) - 1);
         result[i] = chars[random_index];
@@ -252,9 +259,57 @@ char* generate_commit_id() {
     return result;
 }
 void add_to_logs(char* argv[] , const char* commit_id) {
-    FILE* file = fopen(".ginit/logs" , "a");
-    fseek(file , -1 , SEEK_END);
-    char last_line[MAX_LINE_LENGTH];
-    fgets(last_line , sizeof(last_line) , file);
-    printf("%s\n" , last_line);
+    FILE* file = fopen(".ginit/logs" , "a"); FILE* head = fopen(".ginit/HEAD" , "r"); FILE* config = fopen(".ginit/config" , "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        fclose(file); fclose(head); fclose(config);
+        exit(EXIT_FAILURE);
+    }
+    char current_commit_id[9] , current_branch[MAX_BRANCH_NAME] , username[MAX_EMAIL_USERNAME_LENGHT] , email[MAX_EMAIL_USERNAME_LENGHT], line1[MAX_LINE_LENGTH] , line2[MAX_LINE_LENGTH];
+    fgets(line1 , sizeof(line1) , config); fgets(line2 , sizeof(line2) , head);
+    sscanf(line1 , "username : %s email : %s" , username , email); sscanf(line2 , "%s %s" , current_commit_id , current_branch);
+    fprintf(file , "%s %s %s <%s> %ld %s \"%s\"\n" ,current_commit_id , commit_id , username , email , time(NULL) , current_branch , argv[3] );
+    fclose(file); fclose(head); fclose(config);
+}
+void copy_stagedfiles_to_commit_dir(const char* commit_id) {
+    FILE* tracks = fopen(".ginit/tracks" , "r");
+    if (tracks == NULL) {
+        perror("Error opening file");
+        fclose(tracks);
+        exit(EXIT_FAILURE);
+    }
+    char line[MAX_LINE_LENGTH] , name[MAX_FILENAME] , address[MAX_ADDRESS_LENGTH] , cwd[MAX_ADDRESS_LENGTH] ,new_path[MAX_ADDRESS_LENGTH] , new_address[MAX_ADDRESS_LENGTH];
+    getcwd(cwd , sizeof(cwd));
+    int l = strlen(cwd) + 1;
+    while (fgets(line , sizeof(line) , tracks)) {
+        sscanf(line , "%s %s " , name , address);
+        strcpy(new_address , address);
+        memmove(new_address , new_address + l , strlen(new_address) - l + 1);
+        sprintf(new_path , "%s/.ginit/commits/%s/%s" , cwd , commit_id , new_address);
+        if (is_directory(address)) {
+            mkdir(new_path , 0755);
+        }
+        else {
+            FILE* src = fopen(address , "rb+"); 
+            if (src == NULL) {
+                perror("Error opening file");
+                exit(EXIT_FAILURE);
+            }
+            FILE* dest = fopen(new_path , "wb+");
+            if (dest == NULL) {
+                perror("Error opening file");
+                exit(EXIT_FAILURE);
+            }
+            copy_file_source_to_dest(dest , src);
+        }
+    }
+    fclose(tracks);
+}
+int is_directory(const char* path) {
+    struct stat path_stat;
+    if (stat(path , &path_stat)) {
+        perror("Error getting file status");
+        exit(EXIT_FAILURE);
+    }
+    return S_ISDIR(path_stat.st_mode);
 }
